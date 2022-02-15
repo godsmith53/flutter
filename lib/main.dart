a10f1302cfe2ff5e923fbacae8624f2c3a6e59ca
Aditya Verma <toaditya07@gmail.com>
2022-02-16 01:24:22 +0530

    day4

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter1/pages/homepage.dart';
 import 'package:flutter1/pages/loginpage.dart';
+import 'package:google_fonts/google_fonts.dart';
 void main(){
   runApp(MyApp());
 }
@@ -13,14 +14,18 @@ class MyApp extends StatelessWidget {
     return  MaterialApp(
      
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily
+       ),
       darkTheme: ThemeData(
         brightness: Brightness.dark
       ),
+      
       routes: {
         "/":(context)=>loginpage(),
-        "/home":(context)=>homepage(),
-        "/login":(context)=>loginpage(),
+           "/home":(context)=>homepage(),
+              "/login":(context)=>loginpage(),
       },
 
     );

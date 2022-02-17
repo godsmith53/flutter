42ae6ca2ba11d41db8d1b3d678fa94ff3476d9be
Aditya Verma <toaditya07@gmail.com>
2022-02-17 06:07:43 +0530

    day 9 completed added items to the homepage

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter1/models/catalog.dart';
 import 'package:flutter1/widgets/drawer.dart';
+import 'package:flutter1/widgets/item_widget.dart';
 
 class homepage extends StatelessWidget {
   const homepage({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
+  final dummyList=List.generate(20, (index) => CatalogModel.items[0]);
     
     return Scaffold(
       appBar: AppBar(
         
         title: const Text("Catalog App",),
       ),
-      body:Center(child:Container(
-      child: const Text("My First Flutter App :)"),
-    ),
-    ) ,
+      body:Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            return ItemWidget(item:dummyList[index]);
+          },
+          itemCount: dummyList.length,),
+      ),
     drawer: myDrawer(),
     )  
     ;
